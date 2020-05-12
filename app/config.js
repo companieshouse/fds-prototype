cb12970aefc1f3e29f40dee20ef7166d9311e0a9
Michael Gearon <mgearon@companieshouse.gov.uk>
2020-05-12 07:13:02 +0100

    Change service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Forms download service',
+  serviceName: 'Download a Companies House document',
 
   // Default port that prototype runs on
   port: '3000',
